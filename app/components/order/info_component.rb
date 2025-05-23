111bfffdc7a646e3a4ffce00caf9d78593da708b
Matis121 <mateusz6246@gmail.com>
2025-05-24 00:35:12 +0200

    feat: turbo frame on order status

--- app/components/order/info_component.rb
+++ app/components/order/info_component.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Order::InfoComponent < ViewComponent::Base
+  include Turbo::FramesHelper
+
   def initialize(order:, order_statuses:)
     @order = order
     @order_statuses = order_statuses
