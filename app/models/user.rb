7a5dc8e5f76996749685a2c09f021744f33d9213
Matis121 <mateusz6246@gmail.com>
2025-07-11 17:14:07 +0200

    feat(models): add validations and default data creation for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,19 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 2 }
+  validates :password_confirmation, presence: true, length: { minimum: 2 }
+
+  after_create :create_default_data
+
+
+  private
+
+  def create_default_data
+    default_catalog = catalogs.create!(name: "Domyślny")
+    default_catalog.warehouses.create!(name: "Domyślny")
+    default_catalog.price_groups.create!(name: "Domyślny")
+  end
 end
