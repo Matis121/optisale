e3979cdc466e97502fc9047da3a702d49757f44d
Matis121 <mateusz6246@gmail.com>
2025-07-12 00:10:03 +0200

    feat(models): add default warehouse management and prevent deletion of default warehouses

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -4,4 +4,25 @@ class Warehouse < ApplicationRecord
 
   validates :name, presence: true
   validates :catalog_id, presence: true
+
+  before_save :ensure_default_warehouse
+  before_destroy :prevent_destroy_if_default
+
+  private
+
+  def ensure_default_warehouse
+    if default?
+      self.catalog.warehouses
+        .where(default: true)
+        .where.not(id: self.id)
+        .update_all(default: false)
+    end
+  end
+
+  def prevent_destroy_if_default
+    if default?
+      errors.add(:base, "Nie można usunąć domyślnego magazynu")
+      throw :abort
+    end
+  end
 end
