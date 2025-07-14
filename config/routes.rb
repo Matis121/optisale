eee4beb907875e757ffe04fadcaff817784a3e2c
Matis121 <mateusz6246@gmail.com>
2025-07-14 18:53:31 +0200

    style: enhance user authentication views with improved styling and layout, including responsive design and error message formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    confirmations: "users/confirmations",
+    passwords: "users/passwords",
+    sessions: "users/sessions",
+    unlocks: "users/unlocks"
+  }
+
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  root "orders#index"
+
+
   # Storage
   namespace :storage do
     root to: "products#index"
@@ -29,19 +42,4 @@ Rails.application.routes.draw do
 
   # Order products
   resources :order_products
-
-
-  devise_for :users, path: ""
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  root "orders#index"
 end
