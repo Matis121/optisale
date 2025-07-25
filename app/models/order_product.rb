66f45570bffa98c37e5d38ddd284ad6ab40efd13
Matis121 <mateusz6246@gmail.com>
2025-07-26 00:09:28 +0200

    feat: automatic product detail assignment and update total price calculation in order

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -4,4 +4,27 @@ class OrderProduct < ApplicationRecord
 
   validates :quantity, presence: true, numericality: { greater_than: 0 }
   validates :order_id, presence: true
+
+  before_save :set_product_details, if: :product_id_changed?
+
+  private
+
+  def set_product_details
+    return unless product
+
+    self.name = product.name
+    self.sku = product.sku
+    self.ean = product.ean
+    self.tax_rate = product.tax_rate
+    set_prices
+  end
+
+  def set_prices
+    default_price_group = order.user.price_groups.find_by(default: true)
+    product_price = product.product_prices.find_by(price_group: default_price_group) ||
+                   product.product_prices.first
+
+    self.gross_price = product_price&.gross_price || 0
+    self.nett_price = product_price&.nett_price || 0
+  end
 end
