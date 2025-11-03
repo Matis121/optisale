bd87e3dee79e7a93c9c3517dfe5da93a17abcaac
Matis121 <mateusz6246@gmail.com>
2025-11-03 20:19:00 +0100

    refactor: handle turbo frame in price groups

--- app/controllers/storage/price_groups_controller.rb
+++ app/controllers/storage/price_groups_controller.rb
@@ -1,9 +1,9 @@
 class Storage::PriceGroupsController < ApplicationController
   before_action :set_price_group, only: %i[ show edit update destroy ]
   before_action :ensure_turbo_frame, only: %i[ edit new ]
+  before_action :set_price_groups, only: %i[ index create update destroy ]
 
   def index
-    @price_groups = current_user.price_groups
   end
 
   def show
@@ -11,11 +11,11 @@ class Storage::PriceGroupsController < ApplicationController
 
   def new
     @price_group = PriceGroup.new
-    @catalogs = current_user.catalogs
+    set_catalogs
   end
 
   def edit
-    @catalogs = current_user.catalogs
+    set_catalogs
   end
 
   def create
@@ -23,41 +23,45 @@ class Storage::PriceGroupsController < ApplicationController
     @price_group.user = current_user
     @price_group.catalogs = current_user.catalogs.where(id: price_group_params[:catalog_ids])
 
-    respond_to do |format|
       if @price_group.save
-        format.html { redirect_to storage_price_groups_path, notice: "Grupa cenowa została utworzona." }
-        format.json { render :show, status: :created, location: @price_group }
+        flash.now[:success] = "Grupa cenowa została utworzona."
+        update_price_groups_frame_with_flash
       else
-        @catalogs = current_user.catalogs
-        format.turbo_stream { render turbo_stream: turbo_stream.update("price-group-form", partial: "storage/price_groups/form") }
-        format.json { render json: @price_group.errors, status: :unprocessable_entity }
+        set_catalogs
+        render_price_groups_form
       end
-    end
   end
 
   def update
-    respond_to do |format|
-      if @price_group.update(price_group_params)
-        format.html { redirect_to storage_price_groups_path, notice: "Grupa cenowa została zaktualizowana." }
-        format.json { render :show, status: :ok, location: @price_group }
-      else
-        @catalogs = current_user.catalogs
-        format.turbo_stream { render turbo_stream: turbo_stream.update("price-group-form", partial: "storage/price_groups/form") }
-        format.json { render json: @price_group.errors, status: :unprocessable_entity }
-      end
+    if @price_group.update(price_group_params)
+      flash.now[:success] = "Grupa cenowa została zaktualizowana."
+      update_price_groups_frame_with_flash
+    else
+      set_catalogs
+      render_price_groups_form
     end
   end
 
   def destroy
     @price_group.destroy!
 
-    respond_to do |format|
-      format.html { redirect_to storage_price_groups_path, status: :see_other, notice: "Grupa cenowa została usunięta." }
-      format.json { head :no_content }
-    end
+    flash.now[:success] = "Grupa cenowa została usunięta."
+    update_price_groups_frame_with_flash
   end
 
   private
+
+  def set_catalogs
+    @catalogs = current_user.catalogs
+  end
+
+  def update_price_groups_frame_with_flash
+    streams = []
+    streams << turbo_stream.update("modal-frame", "")
+    streams << turbo_stream.update("price_groups_frame", partial: "storage/price_groups/table")
+    streams << turbo_stream.update("flash-messages", partial: "shared/flash_messages")
+    render turbo_stream: streams
+  end
 
   def ensure_turbo_frame
     unless turbo_frame_request?
@@ -65,8 +69,16 @@ class Storage::PriceGroupsController < ApplicationController
     end
   end
 
+  def render_price_groups_form
+    render turbo_stream: turbo_stream.replace("price-group-form", partial: "storage/price_groups/form")
+  end
+
   def set_price_group
     @price_group = current_user.price_groups.find(params[:id])
+  end
+
+  def set_price_groups
+    @price_groups = current_user.price_groups
   end
 
   def price_group_params
