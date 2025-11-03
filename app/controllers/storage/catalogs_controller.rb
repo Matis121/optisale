bdc27767aac7f42eee99ed8cc9d65af49670cbc9
Matis121 <mateusz6246@gmail.com>
2025-11-03 19:22:40 +0100

    refactor: update catalogs to use Turbo Frames

--- app/controllers/storage/catalogs_controller.rb
+++ app/controllers/storage/catalogs_controller.rb
@@ -1,9 +1,9 @@
 class Storage::CatalogsController < ApplicationController
   before_action :set_catalog, only: %i[ show edit update destroy ]
+  before_action :set_catalogs, only: %i[ index create update destroy ]
   before_action :ensure_turbo_frame, only: %i[ edit new ]
 
   def index
-    @catalogs = current_user.catalogs
   end
 
   def show
@@ -20,51 +20,58 @@ class Storage::CatalogsController < ApplicationController
     @catalog = Catalog.new(catalog_params)
     @catalog.user = current_user
 
-    respond_to do |format|
-      if @catalog.save
-        format.html { redirect_to storage_catalogs_path, notice: "Katalog został utworzony." }
-        format.json { render :show, status: :created, location: @catalog }
-      else
-      format.turbo_stream { render turbo_stream: turbo_stream.update("catalog-form", partial: "storage/catalogs/form") }
-        format.json { render json: @catalog.errors, status: :unprocessable_entity }
-      end
+    if @catalog.save
+      flash.now[:success] = "Katalog został utworzony."
+      update_catalogs_frame_with_flash
+    else
+      render_catalogs_form
     end
   end
 
   def update
-    respond_to do |format|
       if @catalog.update(catalog_params)
-        format.html { redirect_to storage_catalogs_path, notice: "Katalog został zaktualizowany." }
-        format.json { render :show, status: :ok, location: @catalog }
+        flash.now[:success] = "Katalog został zaktualizowany."
+        update_catalogs_frame_with_flash
       else
-        format.turbo_stream { render turbo_stream: turbo_stream.update("catalog-form", partial: "storage/catalogs/form") }
-        format.json { render json: @catalog.errors, status: :unprocessable_entity }
+        render_catalogs_form
       end
-    end
   end
 
   def destroy
     @catalog.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to storage_catalogs_path, status: :see_other, notice: "Katalog został usunięty." }
-      format.json { head :no_content }
-    end
+    flash.now[:success] = "Katalog został usunięty."
+    update_catalogs_frame_with_flash
   end
 
   private
 
-    def ensure_turbo_frame
-      unless turbo_frame_request?
-        redirect_to storage_catalogs_path
-      end
-    end
+  def set_catalogs
+    @catalogs = current_user.catalogs
+  end
 
-    def set_catalog
-      @catalog = current_user.catalogs.find(params[:id])
-    end
+  def update_catalogs_frame_with_flash
+    streams = []
+    streams << turbo_stream.update("modal-frame", "")
+    streams << turbo_stream.update("catalogs_frame", partial: "storage/catalogs/table")
+    streams << turbo_stream.update("flash-messages", partial: "shared/flash_messages")
+    render turbo_stream: streams
+  end
 
-    def catalog_params
-      params.require(:catalog).permit(:name, :default)
+  def render_catalogs_form
+    render turbo_stream: turbo_stream.replace("catalog-form", partial: "storage/catalogs/form")
+  end
+
+  def ensure_turbo_frame
+    unless turbo_frame_request?
+      redirect_to storage_catalogs_path
     end
+  end
+
+  def set_catalog
+    @catalog = current_user.catalogs.find(params[:id])
+  end
+
+  def catalog_params
+    params.require(:catalog).permit(:name, :default)
+  end
 end
