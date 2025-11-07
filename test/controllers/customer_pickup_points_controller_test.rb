d60dd62dd518e6bcde0c8c6c25b44625d31d87e3
Matis121 <mateusz6246@gmail.com>
2025-11-07 20:04:05 +0100

    chore: setup RSpec and add User model tests

--- test/controllers/customer_pickup_points_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class CustomerPickupPointsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/order_product_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class OrderProductControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get order_product_create_url
-    assert_response :success
-  end
-end
